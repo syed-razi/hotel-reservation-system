a7fca5c850ba0efe98fbb308e4f04213c4a193d3
Razi <syedru@mcmaster.ca>
2022-03-08 08:53:54 -0500

    Initialize hotel with 3 of each type of room

--- Hotel.java
+++ Hotel.java
@@ -1,3 +1,20 @@
-public class Hotel {
+import java.util.*;
 
+public class Hotel {
+  HashMap<Integer, Room> rooms = new HashMap<>();
+  
+  public Hotel() {
+    Type type = Type.SINGLE;
+    for(int i = 0; i < 12; i++) {
+      if(i >= 3 && i < 6) {
+        type = Type.DOUBLE;
+      } else if(i >= 6 && i < 9) {
+        type = Type.QUEEN;
+      } else if(i >= 9 && i < 12) {
+        type = Type.KING;
+      }
+      Room room = new Room(i, type);
+      rooms.put(i, room);
+    }
+  }
 }
