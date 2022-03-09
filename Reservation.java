70be52cfd76de06f938df53b456061d44cf04667
Razi <syedru@mcmaster.ca>
2022-03-08 21:16:13 -0500

    Add total price for reservation

--- Reservation.java
+++ Reservation.java
@@ -3,10 +3,12 @@ import java.time.LocalDate;
 public class Reservation {
   private LocalDate checkin;
   private LocalDate checkout;
+  private int totalPrice;
 
-  public Reservation(String reservationId, LocalDate checkin, LocalDate checkout) {
+  public Reservation(LocalDate checkin, LocalDate checkout, int totalPrice) {
     this.checkin = checkin;
     this.checkout = checkout;
+    this.totalPrice = totalPrice;
   }
 
   public LocalDate getCheckinDate() {
@@ -15,5 +17,9 @@ public class Reservation {
 
   public LocalDate getCheckoutDate() {
     return checkout;
+  }
+
+  public int getTotalPrice() {
+    return totalPrice;
   }
 }
